88bee632dd454fe7e7d6f4c013157bd72422aa29
Samuel Harding IV <samuelhardingiv@MacBook-Pro.local>
2022-09-27 16:47:23 -0500

    All Files

---
+++ PyPoll.py
@@ -0,0 +1,15 @@
+import os
+
+import csv
+
+file_to_load = os.path.join("election_results.csv")
+file_to_save = os.path.join("Analysis" , "election_results.txt")
+
+
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    headers = next(file_reader)
+    print(headers)
+
+    
